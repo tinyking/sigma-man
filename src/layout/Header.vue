d70f2d31316dd4b474616d975f06406cee8609d5
tinyking <akingchao@qq.com>
2020-07-10 20:18:30 +0800

    update

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -3,18 +3,23 @@
     <a-row class="page">
       <a-col :sm="24" :md="6">
         <a class="logo" href="/">
-          <img alt="logo" src="https://gw.alipayobjects.com/zos/rmsportal/SVDdpZEbAlWBFuRGIIIL.svg">
+          <img alt="logo" src="https://gw.alipayobjects.com/zos/rmsportal/SVDdpZEbAlWBFuRGIIIL.svg" />
           <span>LANDING</span>
         </a>
       </a-col>
       <a-col :sm="0" :md="18">
         <div class="menu">
           <a-menu mode="horizontal">
-            <a-menu-item key="mail"> 首页</a-menu-item>
-            <a-menu-item key="mail2"> 文档</a-menu-item>
-            <a-menu-item key="mail23"> 文档</a-menu-item>
+            <a-menu-item key="mail">
+              <router-link to="/">首页</router-link>
+            </a-menu-item>
+            <a-menu-item key="mail2">
+              <router-link to="/">博客</router-link>
+            </a-menu-item>
+            <a-menu-item key="mail23">
+              <router-link to="/">工具</router-link>
+            </a-menu-item>
           </a-menu>
-          <a href="https://github.com/tinyking" target="_blank" class="gitbtn">Github</a>
           <a href="https://github.com/tinyking" target="_blank" class="gitbtn">Github</a>
         </div>
       </a-col>
@@ -26,10 +31,7 @@
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component
-export default class Header extends Vue {
-};
+export default class Header extends Vue {}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
